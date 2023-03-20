2cfe706bbfd18d62cffa5291b58fa7b332ff7aca
Jean-Francois Labonte <grimsleepless@protonmail.com>
2023-03-19 22:03:06 -0400

    Update dependencies and add an executable to run setup

---
+++ dot_setup/executable_install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sudo apt install -y build-essential git curl
+curl -sS https://starship.rs/install.sh | sh
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew bundle
+
+sudo cp -f hid_apple.conf /etc/modprobe.d/
+sudo update-initramfs -u
