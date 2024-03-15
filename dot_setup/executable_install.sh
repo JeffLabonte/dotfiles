3d86bab91a76ecce56d4652b3bcfa5a4e2bd6f51
Jeff Labonte <jflabonte@poka.io>
2024-03-15 10:16:16 -0400

    Update install script

--- dot_setup/executable_install.sh
+++ dot_setup/executable_install.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-sudo apt install -y build-essential git curl tmux
+sudo apt install -y build-essential git curl tmux wget
 curl -sS https://starship.rs/install.sh | sh
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew bundle
 
-sudo cp -f hid_apple.conf /etc/modprobe.d/
-sudo update-initramfs -u
+if [ -d /etc/modprobe.d/ ]; then
+    sudo cp -f hid_apple.conf /etc/modprobe.d/
+    sudo update-initramfs -u
+fi
